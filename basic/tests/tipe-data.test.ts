1f99e6501c6a892216bc222fd45a2f5fedcf76a1
jeaneselinasm <jeanessmanuhutu@gmail.com>
2025-04-12 21:04:08 +0900

    testing babel and jest compiler

---
+++ basic/tests/tipe-data.test.ts
@@ -0,0 +1,15 @@
+describe('Data Type', function () {
+    it('should must declare', function(){
+       let name : string = "Jeane Selina"
+        let balance : number = 100000
+        let isVip : boolean = true
+
+        console.info(name)
+        console.info(balance)
+        console.info(isVip)
+
+        name = 1000
+        balance = 'Jeane'
+        isVip =1010
+    })
+})
